822ab8a877be8b67e2185a21faf1ddf2784552e0
Azim <Budazimbud>
2025-05-21 16:07:36 +0800

    setup for connect to potgresql

--- django_backend/settings.py
+++ django_backend/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y9$g3ap*^fz(9$rfhybi(_y$@g&@z89$fk=@rwecdwu)ocq1ue'
+# SECRET_KEY = 'django-insecure-y9$g3ap*^fz(9$rfhybi(_y$@g&@z89$fk=@rwecdwu)ocq1ue'
+SECRET_KEY = os.getenv('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -74,8 +78,12 @@ WSGI_APPLICATION = 'django_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),  # Default to 'localhost' if not set
+        'PORT': os.getenv('DB_PORT', '5432'),      # Default to '5432' if not set
     }
 }
 
